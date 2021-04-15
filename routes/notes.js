2f5cf383ac7ba43d84bfd73078605d59380b3daa
Rebecca Howe <Rebeccahowe20@gmail.com>
2021-04-14 19:57:05 -0700

    add get and post routes for notes

---
+++ routes/notes.js
@@ -0,0 +1,29 @@
+const Router = require("express").Router();
+const Note = require("../models/notes");
+
+Router.get("/", async (req, res) =>{
+    try{
+        const notes = await Note.find()
+        res.json(notes)
+        console.log("We're getting all notes from Router.get('/')")
+    }catch(err){
+        res.status(501)
+        res.send("oh no! Guess we'll never know what happened")
+        console.log("Error in the notes get route", err)
+    }
+});
+
+Router.post("/", async (req, res) =>{
+    try{
+        const notes = await Note.create(req.body)
+        res.status(201)
+        res.send(Note._id)
+        console.log("We got a note w/ ", req.body)
+    }catch(err){
+        res.status(501)
+        res.send("oh no! Guess we'll never know what happened")
+        console.log("Error in the notes post route", err)
+    }
+});
+
+module.exports = Router;
