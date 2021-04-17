40f1533af8e5de1d9bcc933f7c0ae1488a36179c
Titina Armstead <71151032+tmarmstead@users.noreply.github.com>
2021-04-16 23:35:19 -0400

    Created save note functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import TabPanel from './components/Tab';
 import SearchPage from './components/pages/SearchPage';
+import NoteSaver from './components/pages/VideoEditor';
 
 function App() {
+  useEffect(() => {
+    fetch("/api/notes").then(res => res.json()).then(res => console.log("Notes are: ", res));
+  })
+
   return (
     <div>
       <TabPanel />
       <Button variant="contained" color="primary">
         Hello World
     </Button>
-    <SearchPage />
+      <SearchPage />
+      <NoteSaver />
     </div>
   );
 }
