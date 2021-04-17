7cbd4b67be3a027a63a39c6174cd618fec4480b0
Rheanne Reyes <rheannemr@gmail.com>
2021-04-17 10:16:26 -0700

    Fix signup form

--- client/src/components/pages/Signup/index.js
+++ client/src/components/pages/Signup/index.js
@@ -21,14 +21,21 @@ export default function FormPropsTextFields() {
           required
           id="outlined-required"
           label="Required"
-          defaultValue="Hello World"
+          defaultValue="First Name"
           variant="outlined"
         />
         <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Last Name"
+          variant="outlined"
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Email Address"
           variant="outlined"
         />
         <TextField
@@ -39,29 +46,10 @@ export default function FormPropsTextFields() {
           variant="outlined"
         />
         <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-        <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
+          id="outlined-password-input"
+          label="Re-type Password"
+          type="password"
+          autoComplete="current-password"
           variant="outlined"
         />
       </div>
