289a75c5f72ad78c7268bdbf4960f5aefdfea775
Titina Armstead <71151032+tmarmstead@users.noreply.github.com>
2021-04-18 17:18:01 -0400

    Imported passport dependencies

--- client/src/components/pages/Signup/index.js
+++ client/src/components/pages/Signup/index.js
@@ -1,75 +1,83 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import PassportInput from "../Passport-Inputs/passport-inputs";
+import Axios from "axios";
+import "./index.css"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-}));
+function Passport() {
+  const [signUpUsername, setSignUpUsername] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
 
-export default function FormPropsTextFields() {
-  const classes = useStyles();
+  const signup = () => {
+    console.log("username is: ", signUpUsername);
+    console.log("password is: ", signUpPassword);
+    Axios({
+      method: "POST",
+      data: {
+        username: signUpUsername,
+        password: signUpPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:3000/signup",
+    }).then((res) => console.log("signup POST console.log of res: ", res));
+  };
+
+  const login = () => {
+    Axios({
+      method: "POST",
+      data: {
+        username: loginUsername,
+        password: loginPassword
+      },
+      withCredentials: true,
+      url: "http://localhost:3001/login",
+    }).then((res) => console.log("login POST console.log of res: ", res));
+  };
+
+  const getUser = () => {
+    Axios({
+      method: "GET",
+      withCredentials: true,
+      url: "http://localhost:3001/user",
+    }).then((res) => console.log("getUser GET console.log of res: ", res));
+  };
+
+
 
   return (
-    <form style={{ marginTop: "20vh" }} className={classes.root} noValidate autoComplete="off">
-      <div>
 
-      <h2>Login</h2>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Email Address"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="outlined"
-        />
-        <h2>Create An Account</h2>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="First Name"
-          variant="outlined"
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Last Name"
-          variant="outlined"
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Email Address"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Re-type Password"
-          type="password"
-          autoComplete="current-password"
-          variant="outlined"
-        />
+
+    <div className="App" style={{ marginTop: "20vh" }}>
+      <div>
+        <PassportInput
+          title="SignUp"
+          usernamePlaceholder="create username"
+          passwordPlaceholder="create password"
+          stateFunctionUsername={e => setSignUpUsername(e.target.value)}
+          stateFunctionPassword={e => setSignUpPassword(e.target.value)}
+          clickHandler={signup} />
+
+        <PassportInput
+          title="Login"
+          usernamePlaceholder="enter username"
+          passwordPlaceholder="enter password"
+          stateFunctionUsername={e => setLoginUsername(e.target.value)}
+          stateFunctionPassword={e => setLoginPassword(e.target.value)}
+          clickHandler={login} />
+
+        <button
+          onClick={getUser}>Find User
+        </button>
+
       </div>
-    </form>
+
+
+
+
+
+    </div>
   );
 }
+
+export default Passport;
