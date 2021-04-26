28eabb603a3d8e8ce8a0a2234866df7731764591
melissa-stott <1princessmelissa@gmail.com>
2021-04-25 21:44:01 -0700

    added save user functionality to the db

--- client/src/components/NewNote/index.js
+++ client/src/components/NewNote/index.js
@@ -1,62 +1,66 @@
-import React, { useState } from "react";
-import { TextField, Button } from "@material-ui/core";
-import NotesTimeline from "../NotesTimeline";
+import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
+import NotesTimeline from '../NotesTimeline';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function NewNote({ triggerReUpload, videoId }) {
-    const [title, setTitle] = useState("");
-    const [body, setBody] = useState("");
-    const handleSave = () => {
-        fetch("/api/notes", {
-            method: "POST",
-            body: JSON.stringify({ title, body, videoId }),
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-        })
-            .then(data => triggerReUpload(data))
-            .catch(err => alert(err));
-        // console.log("data from inside NEWNOTE in VideoEditor folder: ", req.body)
-        setTitle("");
-        setBody("");
-    };
+	const [ title, setTitle ] = useState('');
+	const [ body, setBody ] = useState('');
+	const { user } = useAuth0();
+	const handleSave = () => {
+		console.log(user.name);
+		const name = user.name;
+		fetch('/api/notes', {
+			method: 'POST',
+			body: JSON.stringify({ title, body, videoId, name }),
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((data) => triggerReUpload(data))
+			.catch((err) => alert(err));
+		// console.log("data from inside NEWNOTE in VideoEditor folder: ", req.body)
+		setTitle('');
+		setBody('');
+	};
 
-    return (
-        <div>
-            <TextField
-                value={title}
-                onChange={event => setTitle(event.target.value)}
-                id="filled-full-width"
-                label="Title"
-                style={{ margin: 8 }}
-                placeholder="Enter the note title here"
-                // fullWidth
-                margin="normal"
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                variant="outlined"
-            />
-            <TextField
-                value={body}
-                onChange={event => setBody(event.target.value)}
-                id="filled-full-width"
-                label="Body"
-                style={{ margin: 8 }}
-                placeholder="Enter the note body here"
-                // fullWidth
-                margin="normal"
-                InputLabelProps={{
-                    shrink: true,
-                }}
-                variant="outlined"
-            />
-            <Button onClick={handleSave} variant="contained" color="primary">
-                Save Note
-            </Button>
-            <NotesTimeline videoId={videoId} />
-        </div>
-    );
+	return (
+		<div>
+			<TextField
+				value={title}
+				onChange={(event) => setTitle(event.target.value)}
+				id="filled-full-width"
+				label="Title"
+				style={{ margin: 8 }}
+				placeholder="Enter the note title here"
+				// fullWidth
+				margin="normal"
+				InputLabelProps={{
+					shrink: true
+				}}
+				variant="outlined"
+			/>
+			<TextField
+				value={body}
+				onChange={(event) => setBody(event.target.value)}
+				id="filled-full-width"
+				label="Body"
+				style={{ margin: 8 }}
+				placeholder="Enter the note body here"
+				// fullWidth
+				margin="normal"
+				InputLabelProps={{
+					shrink: true
+				}}
+				variant="outlined"
+			/>
+			<Button onClick={handleSave} variant="contained" color="primary">
+				Save Note
+			</Button>
+			<NotesTimeline videoId={videoId} />
+		</div>
+	);
 }
 
 export default NewNote;
--- client/src/components/VideoPlayer/index.js
+++ client/src/components/VideoPlayer/index.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function VideoPlayer({ videoId, width, height }) {
-    const videoSrc = `https://www.youtube.com/embed/${videoId}`;
+	const videoSrc = `https://www.youtube.com/embed/${videoId}`;
+	const { user, isAuthenticated, isLoading } = useAuth0();
 
-    return (<div>
-        <div className="ui embed">
-            <iframe src={videoSrc} allowFullScreen title="Video player" style={{width: width, height: height, alignItems: "flex-start"}} />
-        </div>
-        {/* <div className="ui segment">
+	return (
+		<div>
+			<div className="ui embed">
+				<iframe
+					src={videoSrc}
+					allowFullScreen
+					title="Video player"
+					style={{ width: width, height: height, alignItems: 'flex-start' }}
+				/>
+			</div>
+			<h1>{user.name}</h1>
+			{/* <div className="ui segment">
             <h4 className="ui header">{video.snippet.title}</h4>
             <p>{video.snippet.description}</p>
         </div> */}
-    </div>
-    
-    )
+		</div>
+	);
 }
 
 export default VideoPlayer;
